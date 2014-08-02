b189e48b78a653d6555763da2972285c76a69219
latelan <coolboy1353@163.com>
2014-08-02 21:52:34 +0800

    修复多余输出问题

--- testvalid/auto.sh
+++ testvalid/auto.sh
@@ -11,6 +11,6 @@ request_url=http://222.24.19.190:8080/portal/index_default.jsp
 logurl=http://222.24.19.190:8080/portal
 post_info="userName=$user&userPwd=$userpwd"
 
-curl -s -c cookie.txt  -A Mozilla/5.0 -H "Accept-Language:zh-CN" $request_url &> /dev/null 
+curl -s -c cookie.txt  -A Mozilla/5.0 -H "Accept-Language:zh-CN" $request_url > /dev/null 
 curl -s -b cookie.txt  -A Mozilla/5.0 -H "Accept-Language:zh-CN" -H "Referer:$request_url" -d $post_info "$logurl/pws?t=li" | grep heartBeatCyc >/dev/null
 echo $?
